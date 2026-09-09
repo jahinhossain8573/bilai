3f95c0c45218bb7d06778e0f6a3e782ca5a2c02f
DESKTOP-78U35O5\REPAIR CELL <abdullahirshadtazwar@gmail.com>
2026-09-10 02:05:10 +0600

    Added Alot of changes

--- app/components/card.tsx
+++ app/components/card.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import catPhoto from "../resources/sample_cat_photo.jpeg";
 export default function Card() {
   return (
-    <div className="border w-fit rounded-2xl">
+    <div className="border w-fit rounded-2xl bg-[#f0eec9] shadow-md">
       <Image
         src={catPhoto}
         width={300}
@@ -20,16 +20,16 @@ export default function Card() {
           {" "}
           {/* Line 2 — Breed, Age, Gender */}
           <li>
-            <span>•</span>
-            <span>Deshi</span>
+            <span></span>
+            <span className="bg-[#a1cf6b] rounded-xl px-2 py-0.5">Deshi</span>
           </li>
           <li>
             <span>•</span>
-            <span>6 months</span>
+            <span> 6 months</span>
           </li>
           <li>
             <span>•</span>
-            <span>Female</span>
+            <span> Female</span>
           </li>
         </ul>
         <div>
@@ -37,7 +37,7 @@ export default function Card() {
           {/* Line 3 — Location + WhatsApp */}
           <div className="flex justify-between">
             <span>📍 Dhanmondi, Dhaka</span>
-            <button className="bg-gray-200 rounded-2xl px-2">
+            <button className="bg-[#FA7D1F] rounded-xl px-5 py-1 text-[#f0eec9] font-poppins font-bold hover:bg-[#FA7D1F]/80">
               Text on WhatsApp
             </button>
           </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,38 @@
 import Link from "next/link";
 import Card from "./components/card";
 
-export default function App() {
+
+
+export default  function App() {
   return (
-    <div className="">
-      <header className="p-5 flex justify-center gap-3 items-center">
+    <div className="bg-[#FEFFEE] ">
+      <header className=" bg-[#f0eec9] p-1.5  flex justify-center gap-4 my-1.5 mx-1 items-center rounded-xl">
         {/* Navbar */}
-        <div>
+        <div className="bg-[#a1cf6b] rounded-xl p-1 flex gap-2 items-center">
+          <img src="/Bilai.png" width={32} height={32}/>
           {/* ADD IN LOGO AND THE "BILAI" TEXT HERE */}
-          <h1 className="text-1xl font-'matcha mint' font-extrabold text-[#212922]">BILAI. </h1>
+          <h1 className="text-1xl font-matcha_mint font-extrabold text-[#212922]">bilai.</h1>
         </div>
         <input
           type="text"
-          className="bg-gray-200 rounded-2xl p-1"
+          className="bg-[#FFFEEE] rounded-2xl p-1"
           placeholder="Search"
         />
         {/* Search */}
-        <Link href="">Home</Link>
-        <Link href="/add_cat" className=" hover:cursor-pointer">
+        <Link href="font-Poppins" className="text-[#212922] font-poppins font-bold hover:cursor-pointer`">
+          Home
+        </Link>
+        <Link href="/add_cat" className="text-[#212922] font-poppins font-bold hover:cursor-pointer">
           Add a Cat
         </Link>
         <div>
           {/* Add in the Profile image here */}
-          <span>Profile</span>
+          <span className="text-[#212922] font-poppins font-bold hover:cursor-pointer">Profile</span>
         </div>
         {/* Search */}
       </header>
-      <div className="p-3">
-        <h1 className="font-poppins font-extrabold text-3xl text-[#212922]">Cats looking for a home</h1>
+      <div className="my-2 mx-4">
+        <h1 className="font-poppins font-bold text-2xl text-[#212922] my-2">Cats looking for a home</h1>
         <div className="flex flex-wrap gap-4 justify-center">
           <Card />
           <Card />

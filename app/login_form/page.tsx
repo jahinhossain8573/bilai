cfb39f2a0a0e37d4b7bc5397438dbb633c155ef0
DESKTOP-78U35O5\REPAIR CELL <abdullahirshadtazwar@gmail.com>
2026-09-11 17:50:41 +0600

    login button

--- app/login_form/page.tsx
+++ app/login_form/page.tsx
@@ -14,11 +14,17 @@ export default function Page() {
           </h1>
         </div>
       </header>
+
       <div className="flex justify-center items-center bg-[url('/Background.png')] bg-cover bg-no-repeat bg-center w-full h-full min-h-screen ">
-        <div className="w-125 p-10 shadow-lg bg-gradient-to-b from-[#ffffff] to-[#f0eec9] rounded-md">
-          <h1 className="font-poppins font-extrabold text-4xl text-[#212922] flex justify-center">
+      <h1 className="font-matcha-mint font-medium lg:text-3xl ">
+        WELCOME BACK
+        <h2></h2>
+      </h1>
+
+        <div className="w-125 p-10 shadow-lg bg-gradient-to-b from-[#fffff6] via-[#fffff6] to-[#ffe4cf] rounded-md">
+          <h1 className="font-poppins font-extrabold lg:text-4xl text-[#212922] flex justify-center">
             Log in to
-            <span className="font-matcha-mint font-normal lg:text-3xl text-[#212922] mx-2 my-2">
+            <span className="font-matcha-mint font-normal lg:text-3xl text-[#212922] mx-2 lg:my-2">
               bilai.
             </span>
           </h1>
@@ -32,28 +38,33 @@ export default function Page() {
           <div>
             <label
               htmlFor="email"
-              className="block text-base mb-2 my-2 font-poppins font-bold"
+              className="block text-base mb-2 my-5 font-poppins font-bold"
             >
               Email
             </label>
             <input
               type="text"
               id="username"
-              className=" bg-[#fffeee00] border border-gray-300 w-full rounded-md"
+              className=" bg-[#fffeee00] border border-black w-full h-8 rounded-md"
             />
             <label
               htmlFor="email"
-              className="block text-base mb-2 my-2 font-poppins font-bold"
+              className="block text-base mb-2 my-5 font-poppins font-bold"
             >
               Password
             </label>
             <input
               type="password"
               id="username"
-              className=" bg-[#fffeee00] border border-gray-300 w-full rounded-md"
+              className=" bg-[#fffeee00] border border-black w-full rounded-md h-8"
             />
+            <p className="">Don&apos;t have an account?{""}
+              <Link href="/signup_form" className="mx-0.5 font-poppins font-medium text-[#080808] hover:underline"> Sign up</Link>
+            </p>
+            <div className="flex justify-center items-center">
+              <button className="mt-5 font-poppins font-bold text-2xl border-2 border-[#FA7D1F] bg-[#FA7D1F] rounded-xl py-1 w-full hover:bg-[#ffb175] hover:text-[#ffffff]"> LOGIN </button>
 
-            <Link href="/signup_form" className="mt-3">Don&apos;t have an account? Sign up</Link>
+            </div>
 
           </div>
         </div>
